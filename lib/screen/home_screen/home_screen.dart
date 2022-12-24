df76f14a950f063dacdb87a1d6111c9794b3ea4e
zubairali001 <zubair.ali@crosscode.dev>
2022-12-24 12:42:23 +0500

    refactor home screen

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,9 +1,10 @@
+import 'package:love_test_app/general_widgets/general_elevated_button.dart';
 import 'package:love_test_app/screen/home_screen/components/my_text_field.dart';
 import 'package:love_test_app/utils/all_utilities.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -11,28 +12,44 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          height: 900,
           decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/images/bg_image.png'),
               fit: BoxFit.cover,
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children:  [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 40),
-                child: Center(
-                  child: Text('Love Test', style: TextStyle(
-                    fontFamily: "BuiltTitlingRg",
-                    fontSize: 500.sp,
-                    color: Colors.white,
-                  )),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40),
+                  child: Center(
+                    child: Text('Love Test',
+                        style: TextStyle(
+                          fontFamily: "BuiltTitlingRg",
+                          fontSize: 500.sp,
+                          color: Colors.white,
+                        )),
+                  ),
                 ),
-              ),
-              const MyTextField(hintText: 'Enter your name',),
-              const MyTextField(hintText: 'His/Her name',),
-            ],
+                MyTextField(
+                  hintText: 'Enter your name',
+                  onChanged: (value) {},
+                ),
+                MyTextField(
+                  hintText: 'His/Her name',
+                  onChanged: (value) {},
+                ),
+                const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 110, vertical: 60),
+                    child: GeneralElevatedButton(
+                      text: 'Start Test',
+                    ))
+              ],
+            ),
           ),
         ),
       ),
