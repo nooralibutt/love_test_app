bb8d3696088d5b959116eb4349c3c5c1e871d794
zubairali001 <zubair.ali@crosscode.dev>
2022-12-26 19:42:22 +0500

    refactor general elevated button

--- lib/general_widgets/general_elevated_button.dart
+++ lib/general_widgets/general_elevated_button.dart
@@ -1,34 +1,34 @@
 import 'package:love_test_app/utils/all_utilities.dart';
 
 class GeneralElevatedButton extends StatelessWidget {
-  final double? buttonHeight;
-  final void Function()? onPressed;
-  final double? fontSize;
-  final Color? backgroundColor;
+  final double buttonHeight;
+  final void Function() onPressed;
+  final double fontSize;
+  final Color backgroundColor;
   final String text;
 
   const GeneralElevatedButton(
       {Key? key,
-      this.buttonHeight,
-      this.onPressed,
-      this.fontSize,
-      this.backgroundColor,
+      required this.buttonHeight,
+      required this.onPressed,
+      required this.fontSize,
+      required this.backgroundColor,
       required this.text})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: buttonHeight ?? 40,
+        height: buttonHeight,
         child: ElevatedButton(
             style: ElevatedButton.styleFrom(
-                backgroundColor: backgroundColor ?? const Color(0xff00d300),
+                backgroundColor: backgroundColor,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10))),
             onPressed: () {},
             child: Text(
               text,
-              style: TextStyle(fontSize: fontSize ?? 20),
+              style: TextStyle(fontSize: fontSize),
             )));
   }
 }
