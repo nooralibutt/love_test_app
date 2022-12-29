f66f1282986f8ea5d7f450c5000d1ce0a09958ae
zubairali001 <zubair.ali@crosscode.dev>
2022-12-29 17:20:51 +0500

    refactor result screen

--- lib/screen/result_screen/result_screen.dart
+++ lib/screen/result_screen/result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:love_test_app/general_widgets/background_image.dart';
 import 'package:love_test_app/general_widgets/general_elevated_button.dart';
+import 'package:love_test_app/screen/home_screen/home_screen.dart';
 import 'package:love_test_app/utils/all_utilities.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -14,22 +15,25 @@ class ResultScreen extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 180.w),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          GeneralElevatedButton(
-              onPressed: () {},
-              fontSize: 120.sp,
-              backgroundColor: const Color(0xff00d300),
-              fontColor: Colors.white,
-              text: 'Share',
-              internalPadding: const EdgeInsets.all(25),
-              buttonWidth: 700.w),
-          SizedBox(height: 80.h),
           GeneralElevatedButton(
               onPressed: () {},
               fontSize: 120.sp,
               backgroundColor: const Color(0xff00c6c6),
               fontColor: Colors.white,
-              text: 'Take Quiz 3',
+              text: 'Take Next Quiz',
+              internalPadding: const EdgeInsets.all(25),
+              buttonWidth: 20.w),
+          SizedBox(height: 190.h),
+          GeneralElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, HomeScreen.routeName);
+              },
+              fontSize: 120.sp,
+              backgroundColor: const Color(0xff00d300),
+              fontColor: Colors.white,
+              text: 'Home Screen',
               internalPadding: const EdgeInsets.all(25),
               buttonWidth: 20.w),
         ],
