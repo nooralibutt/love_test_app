1feb836bdd4f19d3bc160ce9ac453b7af6173ff7
zubairali001 <zubair.ali@crosscode.dev>
2023-01-02 18:06:28 +0500

    update qui model

--- lib/model/quiz_model.dart
+++ lib/model/quiz_model.dart
@@ -12,11 +12,11 @@ class QuizModel {
     QuizModel(
         headingText: 'How often do you text each other?',
         options: ['All the time', 'occasionally', 'we don\'t text'],
-        correctAnswer: ''),
+        correctAnswer: 'All the time'),
     QuizModel(
         headingText: 'Do you  dream about Girlfriend?',
         options: ['All the time', 'No', 'Occasionally'],
-        correctAnswer: ''),
+        correctAnswer: 'All the time'),
     QuizModel(
         headingText: 'why are you taking this quiz',
         options: [
@@ -24,18 +24,38 @@ class QuizModel {
           'I think girlfriend like me',
           'Because I am bored'
         ],
-        correctAnswer: ''),
+        correctAnswer: 'I think girlfriend like me'),
     QuizModel(
         headingText:
             'On a scale of 1 to 10. How similar do you think you and girlfriend are?',
         options: ['8-10', '6-8', '1-5'],
-        correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
-    QuizModel(headingText: '', options: ['', '', ''], correctAnswer: ''),
+        correctAnswer: '8-10'),
+    QuizModel(
+        headingText:
+            'Do you honestly think you are falling in love with girlfriend?',
+        options: ['Yes', 'Kind of', 'No'],
+        correctAnswer: 'Yes'),
+    QuizModel(
+        headingText: 'What do like most about girlfriend?',
+        options: ['Everything', 'Her Personality', 'Her Look'],
+        correctAnswer: 'Everything'),
+    QuizModel(
+        headingText: 'Does girlfriend have your phone number?',
+        options: ['Yeah, girlfriend asked me for it', 'Yes, I gave her', 'No'],
+        correctAnswer: 'Yeah, girlfriend asked me for it'),
+    QuizModel(
+        headingText: 'How many interests do you share with girlfriend?',
+        options: ['0', '1-5', '100+'],
+        correctAnswer: '100+'),
+    QuizModel(
+        headingText: 'If girlfriend asked you out on a date,would you go?',
+        options: ['Yes', 'I\'m not sure', 'No'],
+        correctAnswer: 'Yes'),
+    QuizModel(
+        headingText:
+            'Do you get angry when you miss an opportunity to be with girlfriend?',
+        options: ['Yes', 'Sometimes', 'No'],
+        correctAnswer: 'Yes'),
   ];
 
   static final List<QuizModel> quiz2 = [
