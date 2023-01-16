d6cd9af337c32363df8b75f92325b340f80d2164
zubairali001 <zubair.ali@crosscode.dev>
2023-01-16 15:39:01 +0500

    add ad ids

--- lib/services/ad_id_manager.dart
+++ lib/services/ad_id_manager.dart
@@ -6,22 +6,15 @@ class AdIdManager extends IAdIdManager {
   const AdIdManager();
 
   @override
-  AppAdIds? get admobAdIds => AppAdIds(
-        appId:
-            Platform.isAndroid ? '' : 'ca-app-pub-9376647217016516~7235825406',
-        appOpenId:
-            Platform.isAndroid ? '' : 'ca-app-pub-9376647217016516/3038103637',
-        bannerId:
-            Platform.isAndroid ? '' : 'ca-app-pub-9376647217016516/4368616732',
-        interstitialId:
-            Platform.isAndroid ? '' : 'ca-app-pub-9376647217016516/5664266976',
-        rewardedId:
-            Platform.isAndroid ? '' : 'ca-app-pub-9376647217016516/6024441199',
+  AppAdIds? get admobAdIds => const AppAdIds(
+        appId: 'ca-app-pub-4261801828815757~6027277750',
+        bannerId: 'ca-app-pub-4261801828815757/8461869408',
+        interstitialId: 'ca-app-pub-4261801828815757/9774951071',
       );
 
   @override
   AppAdIds? get unityAdIds => AppAdIds(
-        appId: Platform.isAndroid ? '4380053' : '4702876',
+        appId: Platform.isAndroid ? '5119687' : '5119686',
         bannerId: Platform.isAndroid ? 'Banner_Android' : 'Banner_iOS',
         interstitialId:
             Platform.isAndroid ? 'Interstitial_Android' : 'Interstitial_iOS',
@@ -29,18 +22,17 @@ class AdIdManager extends IAdIdManager {
       );
 
   @override
-  AppAdIds? get appLovinAdIds => AppAdIds(
+  AppAdIds? get appLovinAdIds => const AppAdIds(
         appId:
             'OeKTS4Zl758OIlAs3KQ6-3WE1IkdOo3nQNJtRubTzlyFU76TRWeQZAeaSMCr9GcZdxR4p2cnoZ1Gg7p7eSXCdA',
-        interstitialId:
-            Platform.isAndroid ? '286c73782fce6457' : '705a73e1fb071640',
+        interstitialId: 'fc3539edce1d74a0',
+        bannerId: '5632b62ad7e1e3d5',
       );
 
   @override
-  AppAdIds? get fbAdIds => AppAdIds(
-        appId: '222861849563328',
-        bannerId: Platform.isAndroid ? '' : '222861849563328_491138746068969',
-        interstitialId:
-            Platform.isAndroid ? '' : '222861849563328_222886062894240',
+  AppAdIds? get fbAdIds => const AppAdIds(
+        appId: '1892833024385933',
+        bannerId: '1892833024385933_1892833374385898',
+        interstitialId: '1892833024385933_1892833767719192',
       );
 }
